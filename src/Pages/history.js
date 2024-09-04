ecc24f2fb6eee2288dc98e1ac6e2a36d1fb7a89f
Licethd <liceth.dfe@gmail.com>
2024-09-04 02:51:32 -0400

    history

--- src/Pages/history.js
+++ src/Pages/history.js
@@ -16,7 +16,7 @@ export default class history extends Component {
     componentDidMount() {
         SSocket.sendPromise({
             component: "staff_usuario",
-            type: "getTrabajosProximos",
+            type: "getMisTrabajos",
             key_usuario: Model.usuario.Action.getKey()
         }).then(e => {
             this.setState({ data: e.data })
@@ -103,91 +103,104 @@ export default class history extends Component {
                 overflow: "hidden",
             }}>
                 <Degradado />
-                <SView col={"xs-2 sm-2"} row center>
+                <SView col={"xs-2"} row center>
                     <SView style={{
                         width: 40,
                         height: 40,
-                        borderRadius: 100,
+                        borderRadius: 6,
                         borderWidth: 1,
                         borderColor: STheme.color.darkGray,
                         overflow: "hidden",
                     }}>
-                        <SImage src={SSocket.api.root + "company/" + obj?.company?.key} style={{
+                        <SImage src={SSocket.api.root + "staff_tipo/" + obj?.staff_tipo?.key} style={{
                             resizeMode: "cover",
                         }} />
                     </SView>
                 </SView>
-                <SView col={"xs-10 sm-3.5"} >
-                    <SText fontSize={16}>{obj?.company?.descripcion}</SText>
-                    <SText fontSize={14} color={STheme.color.gray} language={{
-                        es: "Empresa",
-                        en: "Company"
-                    }} />
+                <SView col={"xs-10"} >
+                    <SText fontSize={16}>{obj?.descripcion}</SText>
+                    <SText fontSize={14} color={STheme.color.gray}>{obj?.staff_tipo?.descripcion}</SText>
                 </SView>
-                <SView col={"xs-12 sm-0.5"} height={10} />
-                {(obj?.staff_usuario?.key_usuario_atiende) ? <SView col={"xs-12 sm-6"} row >
-                    <SView col={"xs-2 sm-4"} row center>
-                        <SView width={40} height={40} style={{ borderRadius: 5, overflow: "hidden" }} backgroundColor={STheme.color.darkGray}>
-                            <SImage src={SSocket.api.root + "usuario/" + obj?.staff_usuario?.key_usuario_atiende} width={40} height={40} style={{ resizeMode: 'cover', overflow: "hidden" }} />
-                        </SView>
-                    </SView>
-                    <SView col={"xs-8 sm-8"}  >
-                        <SText>{userCoordinador?.Nombres} {userCoordinador?.Apellidos}</SText>
-                        <SText fontSize={14} color={STheme.color.gray} language={{
-                            es: "Coordinador",
-                            en: "Coordinator"
-                        }} />
-                    </SView>
-                </SView> : null}
-                <SHr height={10} />
-                <SView col={"xs-2"} row center>
-                    <SIcon name={"eventi"} fill={STheme.color.gray} width={15} />
-                </SView>
-                <SView col={"xs-10"}  >
-                    <SText fontSize={16}>{obj?.evento?.descripcion}</SText>
-                </SView>
-                <SHr height={5} />
-                <SView col={"xs-2"} row center>
-                    <SIcon name={"worki"} fill={STheme.color.gray} width={16} />
-                </SView>
-                <SView col={"xs-10"}  >
-                    <SText>{obj?.staff_tipo?.descripcion}</SText>
-                </SView>
-                <SHr height={5} />
+                <SHr height={15} />
                 <SView col={"xs-2"} row center>
                     <SIcon name={"dating"} fill={STheme.color.gray} width={16} />
                 </SView>
                 <SView col={"xs-10"}  >
-                    <SText>{new SDate(obj?.evento?.fecha).toString("yyyy-MM-dd")}</SText>
-                </SView>
-                <SHr height={10} />
-                <SView col={"xs-2"} row center>
-                    <SIcon name={"timeIni"} fill={STheme.color.gray} width={16} />
-                </SView>
-                <SView col={"xs-4"}  >
-                    <SText>{new SDate(obj?.staff?.fecha_inicio).toString("hh:mm:ss")}</SText>
-                </SView>
-                <SView col={"xs-2"} row center>
-                    <SIcon name={"timeFinish"} fill={STheme.color.gray} width={16} />
-                </SView>
-                <SView col={"xs-4"}  >
-                    <SText>{new SDate(obj?.staff?.fecha_fin).toString("hh:mm:ss")}</SText>
-                </SView>
-                <SHr height={5} />
-                <SView col={"xs-2"} row center>
-                    <SIcon name={"asistencia2"} fill={STheme.color.gray} width={16} />
-                </SView>
-                <SView col={"xs-10"}  >
-                    <SText>{obj?.asistencia_staff_usuario ? obj?.asistencia_staff_usuario.length : 0}</SText>
+                    <SText>{new SDate(obj?.fecha).toString("yyyy-MM-dd")}</SText>
                 </SView>
             </SView>
         }} />
+    }
+
+    getResumen() {
+        return <>
+            <SView col={"xs-12"} row>
+                <SView col={"xs-6"}  row style={{paddingRight:5}}>
+                    <SView col={"xs-12"} row style={{
+                        borderWidth: 1,
+                        borderColor: STheme.color.darkGray,
+                        borderRadius: 16,
+                        overflow: "hidden",
+                    }}>
+                        <Degradado />
+                        <SView col={"xs-12"} padding={10}>
+                            <SText fontSize={18} color={STheme.color.text}>
+                                Total events
+                            </SText>
+                        </SView>
+
+                        <SView col={"xs-8"} center padding={10}>
+                            <SText fontSize={30} center>
+                                15
+                            </SText>
+                        </SView>
+                        <SView col={"xs-4"} center style={{
+                            backgroundColor: "#C09C0C",
+                            borderBottomLeftRadius: 50,
+                            borderTopLeftRadius: 50,
+                        }}>
+                            <SIcon name={"hisEvent"} fill={STheme.color.white} width={30} />
+                        </SView>
+                    </SView>
+                </SView>
+                <SView col={"xs-6"}  row style={{paddingLeft:5}}>
+                    <SView col={"xs-12"} row style={{
+                        borderWidth: 1,
+                        borderColor: STheme.color.darkGray,
+                        borderRadius: 16,
+                        overflow: "hidden",
+                    }}>
+                        <Degradado />
+                        <SView col={"xs-12"} padding={10}>
+                            <SText fontSize={18} color={STheme.color.text}>
+                                Completed
+                            </SText>
+                        </SView>
+
+                        <SView col={"xs-8"} center padding={10}>
+                            <SText fontSize={30} center>
+                                12
+                            </SText>
+                        </SView>
+                        <SView col={"xs-4"} center style={{
+                            backgroundColor: "#33BE5B",
+                            borderBottomLeftRadius: 50,
+                            borderTopLeftRadius: 50,
+                        }}>
+                            <SIcon name={"hisCompleted"} fill={STheme.color.white} width={30} />
+                        </SView>
+                    </SView>
+                </SView>
+            </SView>
+        </>
     }
 
     render() {
         return <SPage titleLanguage={{ es: "Mi historial", en: "My history" }} preventBack footer={<PBarraFooter url={'/history'} />}>
             <Container>
                 {this.getPerfil()}
+                <SHr height={25} />
+                {this.getResumen()}
                 <SHr height={25} />
                 {this.getList()}
                 <SHr height={90} />
