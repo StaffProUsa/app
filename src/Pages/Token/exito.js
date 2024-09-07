5f038c57d4cce4211091e4ec4df669cdcdb7f119
Licethd <liceth.dfe@gmail.com>
2024-09-07 00:23:43 -0400

    mensaje asistencia

--- src/Pages/Token/exito.js
+++ src/Pages/Token/exito.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { SPage, SText } from 'servisofts-component';
+import { SGradient, SHr, SIcon, SPage, SText, STheme, SView } from 'servisofts-component';
 import PBarraFooter from '../../Components/PBarraFooter';
 import { Container } from '../../Components';
 
@@ -12,10 +12,52 @@ export default class exito extends Component {
     }
 
     render() {
-        return <SPage footer={<PBarraFooter url={'/token/exito'} />} disableScroll>
-            <Container flex>
-                <SText fontSize={18}>{"EXITO AL MARCAR"}</SText>
-            </Container>
+        return <SPage center title={'Sorry'} disableScroll footer={<PBarraFooter url={'/'} />}>
+            <SView col={'xs-12'} center>
+                <Container>
+                    <SView col={'xs-12'} center
+                        style={{
+                            padding: 20,
+                            borderRadius: 16,
+                            borderWidth: 1,
+                            borderColor: STheme.color.darkGray,
+                            overflow: 'hidden',
+                        }}>
+                        <SGradient colors={["#0C0C10", "#040405"]} style={{ borderRadius: 16, }} />
+                        <SHr height={25} />
+                        <SText fontSize={28} center language={{
+                            es: "¡Inicio de asistencia exitosa!",
+                            en: "Successful start of assistance!"
+                        }} />
+                        <SHr height={35} />
+                        <SView width={140} height={140} center style={{
+                            borderRadius: 130,
+                            backgroundColor: STheme.color.white,
+                            overflow: 'hidden',
+                            borderWidth: 6,
+                            borderColor: STheme.color.success,
+                        }}>
+                            <SIcon name={'ok'} fill={STheme.color.success} height={80} />
+                        </SView>
+                        <SHr height={35} />
+                        <SView col={"xs-12"}  center>
+                            <SText fontSize={20} center language={{
+                                es: "Te deseamos muchos éxitos en tu jornada laboral",
+                                en: "We wish you every success in your working day"
+                            }} />
+                            <SView row  >
+                                <SHr height={10} />
+                                <SIcon name={'aplausos'} fill={STheme.color.text} height={22} width={22} />
+                                <SIcon name={'aplausos'} fill={STheme.color.text} height={22} width={22} />
+                                <SIcon name={'aplausos'} fill={STheme.color.text} height={22} width={22} />
+
+                            </SView>
+                        </SView>
+                        <SHr height={25} />
+                    </SView>
+                </Container>
+                <SHr height={45} />
+            </SView>
         </SPage>
     }
 }
