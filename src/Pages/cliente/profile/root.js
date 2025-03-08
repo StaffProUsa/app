c85507cdd006071d0bb44e8550f0fc9542a685c9
Alvaro Siles Estrada <alvarosiles1@gmail.com>
2025-03-08 01:01:10 -0400

    Add timesheet button to menu (company, client, event )
    update

--- src/Pages/cliente/profile/root.js
+++ src/Pages/cliente/profile/root.js
@@ -11,136 +11,143 @@ import ImageProfile from '../../../Components/ImageProfile';
 import TextVerMas from '../../../Components/TextVerMas';
 
 class index extends DPA.profile {
-    static FOOTER = <>
-        <PBarraFooter style={{
-            position: "absolute",
-            bottom: 0,
-        }} url={'/company'} />
-    </>
-    constructor(props) {
-        super(props, {
-            title: "Client",
-            Parent: Parent,
-            itemType: "2",
-            pageParams: {
-                backAlternative: (opt) => {
-                    if (this.data?.key_company) {
-                        SNavigation.replace("/cliente", { key_company: this.data?.key_company })
-                    } else {
-                        SNavigation.goBack();
-                    }
-                }
-            },
-            excludes: ["key", "key_servicio", "key_usuario", "fecha_on", "key_company", "estado", "latitude", "longitude"]
-        });
-        this.state = {
-            pasadoSelect: true
-        }
+ static FOOTER = <>
+  <PBarraFooter style={{
+   position: "absolute",
+   bottom: 0,
+  }} url={'/company'} />
+ </>
+ constructor(props) {
+  super(props, {
+   title: "Client",
+   Parent: Parent,
+   itemType: "2",
+   pageParams: {
+    backAlternative: (opt) => {
+     if (this.data?.key_company) {
+      SNavigation.replace("/cliente", { key_company: this.data?.key_company })
+     } else {
+      SNavigation.goBack();
+     }
     }
-    $allowEdit() {
-        this.data = this.$getData();
-        if (!this.data) return "cargando"
-        return Model.usuarioPage.Action.getPermiso({ url: Parent.path, permiso: "edit", user_data: { key_company: this.data.key_company } })
-    }
-    $allowDelete() {
-        this.data = this.$getData();
-        if (!this.data) return "cargando"
-        return Model.usuarioPage.Action.getPermiso({ url: Parent.path, permiso: "delete", user_data: { key_company: this.data.key_company } })
-    }
-    $allowAccess() {
-        this.data = this.$getData();
-        if (!this.data) return "cargando"
-        return Model.usuarioPage.Action.getPermiso({ url: Parent.path, permiso: "ver", user_data: { key_company: this.data.key_company } })
-    }
-    $getData() {
-        // this.usuarios = Model.usuario.Action.getAll();
-        return Parent.model.Action.getByKey(this.pk);
-    }
-    onEdit() {
-        SNavigation.navigate("/cliente/add", { pk: this.pk })
-    }
+   },
+   excludes: ["key", "key_servicio", "key_usuario", "fecha_on", "key_company", "estado", "latitude", "longitude"]
+  });
+  this.state = {
+   pasadoSelect: true
+  }
+ }
+ $allowEdit() {
+  this.data = this.$getData();
+  if (!this.data) return "cargando"
+  return Model.usuarioPage.Action.getPermiso({ url: Parent.path, permiso: "edit", user_data: { key_company: this.data.key_company } })
+ }
+ $allowDelete() {
+  this.data = this.$getData();
+  if (!this.data) return "cargando"
+  return Model.usuarioPage.Action.getPermiso({ url: Parent.path, permiso: "delete", user_data: { key_company: this.data.key_company } })
+ }
+ $allowAccess() {
+  this.data = this.$getData();
+  if (!this.data) return "cargando"
+  return Model.usuarioPage.Action.getPermiso({ url: Parent.path, permiso: "ver", user_data: { key_company: this.data.key_company } })
+ }
+ $getData() {
+  // this.usuarios = Model.usuario.Action.getAll();
+  return Parent.model.Action.getByKey(this.pk);
+ }
+ onEdit() {
+  SNavigation.navigate("/cliente/add", { pk: this.pk })
+ }
 
-    onChangeLanguage(language) {
-        this.setState({ ...this.state })
-    }
-    componentDidMount() {
-        // this.data = this.$getData();
+ onChangeLanguage(language) {
+  this.setState({ ...this.state })
+ }
+ componentDidMount() {
+  // this.data = this.$getData();
 
-        SLanguage.addListener(this.onChangeLanguage.bind(this))
-        SSocket.sendPromise({
-            component: "evento",
-            type: "getEstadoEventos",
-            key_cliente: this.pk,
-        }).then(e => {
-            console.log(e);
-        }).catch(e => {
-            console.error(e);
-        })
-    }
-    componentWillUnmount() {
-        SLanguage.removeListener(this.onChangeLanguage)
-    }
+  SLanguage.addListener(this.onChangeLanguage.bind(this))
+  SSocket.sendPromise({
+   component: "evento",
+   type: "getEstadoEventos",
+   key_cliente: this.pk,
+  }).then(e => {
+   console.log(e);
+  }).catch(e => {
+   console.error(e);
+  })
+ }
+ componentWillUnmount() {
+  SLanguage.removeListener(this.onChangeLanguage)
+ }
 
-    $render() {
-        this.data = this.$getData();
-        return <SView col={"xs-12"} card center padding={8}>
-            <SView row center>
-                <ImageProfile
-                    width={60}
-                    src={SSocket.api.root + "cliente/" + this.pk}
-                />
-                <SView width={4} />
-                <SView>
-                    <ImageProfile src={SSocket.api.root + "company/" + this.data?.key_company} />
-                    <SText fontSize={18} bold>{this.data?.descripcion}</SText>
-                </SView>
-            </SView>
-            <TextVerMas limitString={200} col={"xs-12"} center color={STheme.color.gray}>{this.data?.observacion}</TextVerMas>
-            {/* <SText col={"xs-12"} center color={STheme.color.gray}>{SUtil.limitString(this.data?.observacion, 200)}</SText> */}
-        </SView>
-    }
-    $footer() {
-        return <SView col={"xs-12"}>
-            <SHr />
-            <MenuPages path='/cliente/profile' permiso='ver'>
-                <MenuButtom
-                    icon={<SIcon name='Excel' />}
-                    label={SLanguage.select({
-                        en: "Attendace Report",
-                        es: "Reporte de horas trabajadas"
-                    })} url='/cliente/profile/hours' params={{ pk: this.pk }} />
-            </MenuPages>
-            <SHr />
-            <SView row col={"xs-12"} center>
-                <SText language={{ en: "Events", es: "Eventos" }} fontSize={16} bold flex />
-                <SView height={30} backgroundColor={this.state.pasadoSelect ? STheme.color.success : STheme.color.lightGray} center padding={8} style={{
-                    borderTopLeftRadius: 7,
-                    borderBottomLeftRadius: 7,
-                }}
-                    onPress={() => {
-                        this.setState({ pasadoSelect: true })
-                    }}>
-                    <SText language={{ en: "Ongoing", es: "En curso" }} fontSize={12} bold color={this.state.pasadoSelect ? STheme.color.white : "#646464"} />
-                </SView>
-                <SView height={30} backgroundColor={this.state.pasadoSelect ? STheme.color.lightGray : STheme.color.success} center padding={8} style={{
-                    borderTopRightRadius: 7,
-                    borderBottomRightRadius: 7,
-                }}
-                    onPress={() => {
-                        this.setState({ pasadoSelect: false })
-                    }}>
-                    <SText language={{ en: "Past", es: "Pasados" }} fontSize={12} bold color={this.state.pasadoSelect ? "#646464" : STheme.color.white} />
-                </SView>
-                <SView width={5} />
-                <SView width={30} height={30} onPress={() => {
-                    SNavigation.navigate("/evento/registro", { key_cliente: this.data.key, key_company: this.data.key_company })
-                }}>
-                    <SIcon name='Add' />
-                </SView>
-            </SView>
-            <SHr />
-            <Eventos key_cliente={this.pk} pasadoSelect={this.state.pasadoSelect} />
-        </SView>
-    }
+ $render() {
+  this.data = this.$getData();
+  return <SView col={"xs-12"} card center padding={8}>
+   <SView row center>
+    <ImageProfile
+     width={60}
+     src={SSocket.api.root + "cliente/" + this.pk}
+    />
+    <SView width={4} />
+    <SView>
+     <ImageProfile src={SSocket.api.root + "company/" + this.data?.key_company} />
+     <SText fontSize={18} bold>{this.data?.descripcion}</SText>
+    </SView>
+   </SView>
+   <TextVerMas limitString={200} col={"xs-12"} center color={STheme.color.gray}>{this.data?.observacion}</TextVerMas>
+   {/* <SText col={"xs-12"} center color={STheme.color.gray}>{SUtil.limitString(this.data?.observacion, 200)}</SText> */}
+  </SView>
+ }
+ $footer() {
+  return <SView col={"xs-12"}>
+   <SHr />
+   <MenuPages path='/cliente/profile' permiso='ver'>
+    <MenuButtom
+     icon={<SIcon name='Excel' />}
+     label={SLanguage.select({
+      en: "Attendace Report",
+      es: "Reporte de horas trabajadas"
+     })} url='/cliente/profile/hours' params={{ pk: this.pk }} />
+
+
+    <MenuButtom
+     icon={<SIcon name='Menu' />}
+     label={SLanguage.select({ en: "timeSheets", es: "nuevo timeSheets" })} url='/company/timeSheets'
+     params={{ key_company: this.data?.key_company, key_cliente: this.pk }} />
+
+   </MenuPages>
+   <SHr />
+   <SView row col={"xs-12"} center>
+    <SText language={{ en: "Events", es: "Eventos" }} fontSize={16} bold flex />
+    <SView height={30} backgroundColor={this.state.pasadoSelect ? STheme.color.success : STheme.color.lightGray} center padding={8} style={{
+     borderTopLeftRadius: 7,
+     borderBottomLeftRadius: 7,
+    }}
+     onPress={() => {
+      this.setState({ pasadoSelect: true })
+     }}>
+     <SText language={{ en: "Ongoing", es: "En curso" }} fontSize={12} bold color={this.state.pasadoSelect ? STheme.color.white : "#646464"} />
+    </SView>
+    <SView height={30} backgroundColor={this.state.pasadoSelect ? STheme.color.lightGray : STheme.color.success} center padding={8} style={{
+     borderTopRightRadius: 7,
+     borderBottomRightRadius: 7,
+    }}
+     onPress={() => {
+      this.setState({ pasadoSelect: false })
+     }}>
+     <SText language={{ en: "Past", es: "Pasados" }} fontSize={12} bold color={this.state.pasadoSelect ? "#646464" : STheme.color.white} />
+    </SView>
+    <SView width={5} />
+    <SView width={30} height={30} onPress={() => {
+     SNavigation.navigate("/evento/registro", { key_cliente: this.data.key, key_company: this.data.key_company })
+    }}>
+     <SIcon name='Add' />
+    </SView>
+   </SView>
+   <SHr />
+   <Eventos key_cliente={this.pk} pasadoSelect={this.state.pasadoSelect} />
+  </SView>
+ }
 }
 export default connect(index);
--- src/Pages/company/profile/root.js
+++ src/Pages/company/profile/root.js
@@ -9,87 +9,96 @@ import Roles from '../../../Roles';
 import PBarraFooter from '../../../Components/PBarraFooter';
 
 class index extends DPA.profile {
-    static FOOTER = <>
-        <PBarraFooter style={{
-            position: "absolute",
-            bottom: 0,
-        }} url={'/company'} />
-    </>
+ static FOOTER = <>
+  <PBarraFooter style={{
+   position: "absolute",
+   bottom: 0,
+  }} url={'/company'} />
+ </>
 
-    constructor(props) {
-        super(props, { Parent: Parent, excludes: ["key", "key_usuario", "key_servicio", "estado", "fecha_on",] });
-    }
-    state = {
-    }
-    onChangeLanguage(language) {
-        this.setState({ ...this.state })
-    }
-    componentDidMount() {
-        SLanguage.addListener(this.onChangeLanguage.bind(this))
-        // Roles.$getPermiso({
-        //     url: Parent.path, permiso: "ver", key_company: this.pk, key_usuario: Model.usuario.Action.getKey()
-        // }).then(e => {
-        //     this.setState({ permiso_ver: "si" })
-        // }).catch(e => {
-        //     this.setState({ permiso_ver: "" })
-        // })
-    }
-    componentWillUnmount() {
-        SLanguage.removeListener(this.onChangeLanguage)
-    }
-    
-    $allowEdit() {
-        return Model.usuarioPage.Action.getPermiso({ url: Parent.path, permiso: "edit", user_data: { key_company: this.pk } })
-    }
-    $allowDelete() {
-        return Model.usuarioPage.Action.getPermiso({ url: Parent.path, permiso: "delete", user_data: { key_company: this.pk } })
-    }
-    $allowAccess() {
-        return Model.usuarioPage.Action.getPermiso({ url: Parent.path, permiso: "ver", user_data: { key_company: this.pk } })
-    }
+ constructor(props) {
+  super(props, { Parent: Parent, excludes: ["key", "key_usuario", "key_servicio", "estado", "fecha_on",] });
+ }
+ state = {
+ }
+ onChangeLanguage(language) {
+  this.setState({ ...this.state })
+ }
+ componentDidMount() {
+  SLanguage.addListener(this.onChangeLanguage.bind(this))
+  // Roles.$getPermiso({
+  //     url: Parent.path, permiso: "ver", key_company: this.pk, key_usuario: Model.usuario.Action.getKey()
+  // }).then(e => {
+  //     this.setState({ permiso_ver: "si" })
+  // }).catch(e => {
+  //     this.setState({ permiso_ver: "" })
+  // })
+ }
+ componentWillUnmount() {
+  SLanguage.removeListener(this.onChangeLanguage)
+ }
 
-    $getData() {
-        this.usuarios = Model.usuario.Action.getAll();
-        return Parent.model.Action.getByKey(this.pk);
-    }
+ $allowEdit() {
+  return Model.usuarioPage.Action.getPermiso({ url: Parent.path, permiso: "edit", user_data: { key_company: this.pk } })
+ }
+ $allowDelete() {
+  return Model.usuarioPage.Action.getPermiso({ url: Parent.path, permiso: "delete", user_data: { key_company: this.pk } })
+ }
+ $allowAccess() {
+  return Model.usuarioPage.Action.getPermiso({ url: Parent.path, permiso: "ver", user_data: { key_company: this.pk } })
+ }
 
-    $menu() {
-        var items = super.$menu();
-        if (Model.usuarioPage.Action.getPermiso({ url: Parent.path, permiso: "share", user_data: { key_company: this.pk } })) {
-            items.push({
-                children: <SView col={"xs-12"} center padding={10} row
-                    onPress={() => {
-                        SNavigation.navigate("/company/invite", { key_company: this.pk })
-                    }}>
-                    <SIcon name={"Compartir"} height={15} width={15} fill={STheme.color.text} />
-                    <SView width={10} />
-                    <SText>Share</SText>
-                </SView>,
-            })
-        }
+ $getData() {
+  this.usuarios = Model.usuario.Action.getAll();
+  return Parent.model.Action.getByKey(this.pk);
+ }
 
-        return items;
-    }
+ $menu() {
+  var items = super.$menu();
+  if (Model.usuarioPage.Action.getPermiso({ url: Parent.path, permiso: "share", user_data: { key_company: this.pk } })) {
+   items.push({
+    children: <SView col={"xs-12"} center padding={10} row
+     onPress={() => {
+      SNavigation.navigate("/company/invite", { key_company: this.pk })
+     }}>
+     <SIcon name={"Compartir"} height={15} width={15} fill={STheme.color.text} />
+     <SView width={10} />
+     <SText>Share</SText>
+    </SView>,
+   })
+  }
 
-    $footer() {
-        
-        return <SView col={"xs-12"}>
-            <SHr />
-            <MenuPages path='/company/' permiso='ver' params={{ pk: this.pk }} blackList={[
-                "/company/profile/staff_tipo",
-                "/company/profile/users",
-                "/company/profile/report",
-            ]}>
-                <MenuButtom label={SLanguage.select({ en: "Client", es: "Cliente" })} url='/cliente' params={{ key_company: this.pk }} icon={<SIcon name='ubicacionesA' />} />
-                <MenuButtom label={SLanguage.select({ en: "Type Staff", es: "Staff Tipo" })} url='/company/profile/staff_tipo' params={{ pk: this.pk }} icon={<SIcon name='staffTipoA' />} />
-                <MenuButtom label={SLanguage.select({ en: "Users", es: "Usuarios" })} url='/company/profile/users' params={{ pk: this.pk }} icon={<SIcon name='usuariosA' />} />
-                <SHr />
-                <MenuButtom label={SLanguage.select({ en: "Report", es: "Reporte" })} url='/company/profile/report' params={{ pk: this.pk }} icon={<SIcon name='Excel' />} />
-                {/* <MenuButtom label='Eventos' url='/company/eventos' params={{ key_company: this.pk }} icon={<SIcon name='eventA' />} /> */}
+  return items;
+ }
 
-            </MenuPages>
-        </SView>
-    }
+ $footer() {
+
+  return <SView col={"xs-12"}>
+   <SHr />
+   <MenuPages path='/company/' permiso='ver' params={{ pk: this.pk }} blackList={[
+    "/company/profile/staff_tipo",
+    "/company/profile/users",
+    "/company/profile/report",
+   ]}>
+    <MenuButtom label={SLanguage.select({ en: "Client", es: "Cliente" })} url='/cliente' params={{ key_company: this.pk }} icon={<SIcon name='ubicacionesA' />} />
+    <MenuButtom label={SLanguage.select({ en: "Type Staff", es: "Staff Tipo" })} url='/company/profile/staff_tipo' params={{ pk: this.pk }} icon={<SIcon name='staffTipoA' />} />
+    <MenuButtom label={SLanguage.select({ en: "Users", es: "Usuarios" })} url='/company/profile/users' params={{ pk: this.pk }} icon={<SIcon name='usuariosA' />} />
+    <SHr />
+    <MenuButtom label={SLanguage.select({ en: "Report", es: "Reporte" })} url='/company/profile/report' params={{ pk: this.pk }} icon={<SIcon name='Excel' />} />
+
+
+
+    {/* para evento */}
+    <MenuButtom label={SLanguage.select({ en: "timeSheets", es: "timeSheets" })} url='/company/timeSheets' params={{ key_company: this.pk }} icon={<SIcon name='Menu' />} />
+
+    {/* <MenuButtom label={SLanguage.select({ en: "time_sheets", es: "time_sheets" })} url='/company/profile/time_sheets' params={{ key_company: this.pk, key_cliente: this.pk }} icon={<SIcon name='Menu' />} /> */}
+    {/* <MenuButtom label={SLanguage.select({ en: "time_sheets", es: "time_sheets" })} url='/company/profile/time_sheets' params={{ key_company: this.pk, key_cliente: this.pk }} icon={<SIcon name='Menu' />} /> */}
+
+    {/* <MenuButtom label='Eventos' url='/company/eventos' params={{ key_company: this.pk }} icon={<SIcon name='eventA' />} /> */}
+
+   </MenuPages>
+  </SView>
+ }
 
 }
 export default connect(index);
