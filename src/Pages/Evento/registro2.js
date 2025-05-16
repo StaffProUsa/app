31644f538eb82f271b815aad10e48c9796f7f157
rickypazd <35882906+rickypazd@users.noreply.github.com>
2025-05-16 15:35:27 -0400

    ocoom

--- src/Pages/Evento/registro2.js
+++ src/Pages/Evento/registro2.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { SButtom, SDate, SHr, SInput, SLanguage, SNavigation, SPage, SText, STheme, SView } from 'servisofts-component';
+import { SButtom, SDate, SHr, SInput, SLanguage, SNavigation, SNotification, SPage, SText, STheme, SView } from 'servisofts-component';
 import { Container } from '../../Components';
 import InputFecha from '../../Components/NuevoInputs/InputFecha';
 import InputSelect from '../../Components/NuevoInputs/InputSelect';
@@ -8,6 +8,9 @@ import Input from '../../Components/Input';
 import InputFloat from '../../Components/NuevoInputs/InputFloat';
 import InputHora from '../../Components/NuevoInputs/InputHora';
 import SIconApp from '../../Assets/SIconApp';
+import Model from '../../Model';
+import SSocket from 'servisofts-socket';
+import all from '../usuario/all';
 
 export default class registro2 extends Component {
 
@@ -15,19 +18,187 @@ export default class registro2 extends Component {
     key_cliente = SNavigation.getParam('key_cliente');
 
     state = {
-
         bookings: [
-            {}
+            this.createDefaultStaff(),
         ],
     }
-    data = {
+
+    data = {}
+    handlePress() {
+        if (this.inpDescripcion.verify() == false) return;
+        const event = {
+            "descripcion": this.inpDescripcion.getValue(),
+            "observacion": this.inpObservacion.getValue(),
+            "key_cliente": this.key_cliente,
+            "key_company": this.key_company,
+            "fecha": this.inpFecha.getValue(),
+            // "staff": this.state.bookings
+        }
+
+        let valid = true;
+        const all_staff = this.state.bookings.map((item, index) => {
+            if (item._ref) {
+                if (!item._ref.verify()) {
+                    valid = false;
+                }
+            }
+
+            const fecha_inicio = event.fecha + " " + new SDate(item.fecha_inicio, "yyyy-MM-ddThh:mm:ss").toString("hh:mmTZD");
+            let fecha_fin = "";
+            if (!item.fecha_fin) {
+                fecha_fin = new SDate(fecha_inicio, "yyyy-MM-ddThh:mm:ss").addDay(1).toString("yyyy-MM-dd hh:mmTZD");
+            } else {
+                fecha_fin = event.fecha + " " + new SDate(item.fecha_fin, "yyyy-MM-ddThh:mm:ss").toString("hh:mmTZD");
+            }
+            console.log("fecha_inicio", fecha_inicio);
+            console.log("fecha_fin", fecha_fin);
+            // if(!item.fecha_fin)
+            return {
+                "descripcion": item.descripcion,
+                "observacion": item.observacion,
+                "key_evento": "this.state.key_evento",
+                "key_staff_tipo": item.key_staff_tipo,
+                "fecha_inicio": fecha_inicio,
+                "fecha_fin": fecha_fin,
+                "cantidad": item.cantidad,
+                "nivel_ingles": item.nivel_ingles
+            }
+            // const fecha_fin = new SDate(item.fecha_fin, "yyyy-MM-ddThh:mm:ss");
+
+
+        })
+        if (!valid) {
+            SNotification.send({
+                title: "Error",
+                body: "Complete todos los campos",
+                color: STheme.color.danger,
+                time: 5000,
+            })
+            return;
+        }
+
+        this.registrarEventoConStaff(event, all_staff);
+        // SSocket.sendPromise({
+        //     component: "evento",
+        //     type: "registro",
+        //     data: event,
+        //     key_usuario: Model.usuario.Action.getKey(),
+        // }).then(e => {
+        //     all_staff.forEach((item, index) => {
+        //         item.key_evento = e.data.key;
+        //         SSocket.sendPromise({
+        //             component: "staff",
+        //             type: "registro",
+        //             data: item,
+        //             key_usuario: Model.usuario.Action.getKey(),
+        //         }).then(e => {
+        //             console.log("staff registrado", e);
+        //             SNotification.send({
+        //                 title: "Success",
+        //                 body: "Staff registrado",
+        //                 color: STheme.color.success,
+        //                 time: 5000,
+        //             })
+        //         }).catch(e=>{
+        //             console.log(e);
+        //             SNotification.send({
+        //                 title: "Error",
+        //                 body: "Error al registrar el staff",
+        //                 color: STheme.color.danger,
+        //                 time: 5000,
+        //             })
+        //         })
+
+        //     })
+        //     console.log(e);
+        // }).catch(e => {
+        //     console.log(e);
+        //     SNotification.send({
+        //         title: "Error",
+        //         body: "Error al registrar el evento",
+        //         color: STheme.color.danger,
+        //         time: 5000,
+        //     })
+        // })
+
+        console.log(event, all_staff);
 
     }
-    handlePress() {
 
+    registrarEventoConStaff = async (event, all_staff) => {
+        try {
+            const eventoRes = await SSocket.sendPromise({
+                component: "evento",
+                type: "registro",
+                data: event,
+                key_usuario: Model.usuario.Action.getKey(),
+            });
+
+            console.log("Evento registrado", eventoRes);
+            const key_evento = eventoRes.data.key;
+
+            for (const item of all_staff) {
+                item.key_evento = key_evento;
+                try {
+                    const res = await SSocket.sendPromise({
+                        component: "staff",
+                        type: "registro",
+                        data: item,
+                        key_usuario: Model.usuario.Action.getKey(),
+                    });
+                    console.log("Staff registrado", res);
+                    SNotification.send({
+                        title: "Success",
+                        body: "Staff registrado",
+                        color: STheme.color.success,
+                        time: 5000,
+                    });
+                } catch (err) {
+                    console.log("Error al registrar staff", err);
+                    SNotification.send({
+                        title: "Error",
+                        body: "Error al registrar el staff",
+                        color: STheme.color.danger,
+                        time: 5000,
+                    });
+                }
+            }
+
+            // Una vez que todos los staff están registrados, navegar
+            // SNavigation.goBack();
+            SNavigation.replace("/company/event", { key_evento: key_evento })
+
+        } catch (err) {
+            console.log("Error al registrar evento", err);
+            SNotification.send({
+                title: "Error",
+                body: "Error al registrar el evento",
+                color: STheme.color.danger,
+                time: 5000,
+            });
+        }
+    };
+
+    createDefaultStaff() {
+        return {
+            "cantidad": 1,
+            "descripcion": "",
+            "descripcion_staff_tipo": "",
+            "hora_fin": "",
+            "hora_inicio": "",
+            "fecha_fin": "",
+            "fecha_inicio": "",
+            "key_staff_tipo": "",
+            "nivel_ingles": "NONE",
+            "observacion": "",
+
+        }
     }
     render() {
-        return <SPage title={"Registro 2"}>
+        return <SPage titleLanguage={{
+            es: "Registro de evento",
+            en: "Event registration"
+        }}>
             <Container>
                 <SView col={"xs-12"}>
                     <SHr h={16} />
@@ -36,36 +207,49 @@ export default class registro2 extends Component {
                     <InputFecha ref={ref => this.inpFecha = ref}
                         defaultValue={new SDate(this.data['fecha'], "yyyy-MM-ddThh:mm:ss").toString("yyyy-MM-dd") ?? new SDate().toString("yyyy-MM-dd")}
                         onChange={(e) => {
-                            console.log(e);
+                            this.
+                                console.log(e);
                         }} />
 
-                    <SInput label={SLanguage.select({
-                        es: "Nombre del evento",
-                        en: "Event name"
-                    })} />
-                    <SInput label={SLanguage.select({
-                        es: "Información sobre el evento",
-                        en: "Information about the event"
-                    })} type='textArea' />
+                    <SInput required
+                        ref={ref => this.inpDescripcion = ref}
+                        label={SLanguage.select({
+                            es: "Nombre del evento",
+                            en: "Event name"
+                        })} />
+                    <SInput
+                        ref={ref => this.inpObservacion = ref}
+                        label={SLanguage.select({
+                            es: "Información sobre el evento",
+                            en: "Information about the event"
+                        })} type='textArea' />
                     <SHr h={64} />
                     <SView col={"xs-12"} row>
                         <SText fontSize={18} flex bold underLine language={{ en: "Booking", es: "Reclutas" }} />
-                        <SView width={30} height={30} onPress={() => {
+                        {/* <SView width={30} height={30} onPress={() => {
                             this.state.bookings.push({});
                             this.setState({ bookings: this.state.bookings })
                         }} >
                             <SIconApp name='Add' />
-                        </SView>
+                        </SView> */}
                     </SView>
                     {this.state.bookings.map((item, index) => <InputPosition
-                        key={index+"booking"}
+                        key={index + "booking"}
                         key_company={this.key_company}
+                        staff={item}
+                        ref={ref => item._ref = ref}
                         onDelete={() => {
                             this.state.bookings.splice(index, 1);
                             this.setState({ bookings: this.state.bookings })
                         }}
                     />)}
-
+                    <SHr />
+                    <SView width={30} height={30} onPress={() => {
+                        this.state.bookings.push(this.createDefaultStaff());
+                        this.setState({ bookings: this.state.bookings })
+                    }} >
+                        <SIconApp name='Add' />
+                    </SView>
                 </SView>
                 <SHr h={64} />
                 <SButtom onPress={this.handlePress.bind(this)} type='secondary'>
@@ -162,7 +346,50 @@ class InputPosition extends Component {
         return filtered;
     }
 
+    verify() {
+        let valid = true;
+
+        if (!this._ref["tipo"].verify()) {
+            valid = false;
+        }
+        if (!this._ref["cantidad"].verify()) {
+            valid = false;
+        }
+        if (!this._ref["nivel_ingles"].verify()) {
+            valid = false;
+        }
+        if (!this._ref["hora_inicio"].verify()) {
+            valid = false;
+        }
+        if (!this._ref["hora_fin"].verify()) {
+            valid = false;
+        }
+        if (!this._ref["descripcion"].verify()) {
+            valid = false;
+        }
+
+        return valid;
+
+        // this._ref["cantidad"].verify();
+        // this._ref["nivel_ingles"].verify();
+        // this._ref["hora_inicio"].verify();
+        // this._ref["hora_fin"].verify();
+        // this._ref["descripcion"].verify();
+
+    }
+
     render() {
+
+        // const a = {
+        //     "descripcion": val.descripcion,
+        //     "observacion": val.observacion,
+        //     "key_evento": this.state.key_evento,
+        //     "key_staff_tipo": dataTipo.key,
+        //     "fecha_inicio": fecha_inicio.toString("yyyy-MM-dd hh:mmTZD"),
+        //     "fecha_fin": fecha_fin.toString("yyyy-MM-dd hh:mmTZD"),
+        //     "cantidad": val.cantidad,
+        //     "nivel_ingles": val.nivel_ingles
+        // }
         return <SView col={"xs-12"} row style={{
             justifyContent: "space-between",
             alignItems: "center",
@@ -183,13 +410,16 @@ class InputPosition extends Component {
                     en: "Staff Type"
                 })}
                 required
-                
+                value={this.props.staff.descripcion_staff_tipo}
                 onPress={() => {
                     SNavigation.navigate("/staff_tipo", {
                         key_company: this.props.key_company, onSelect: (e) => {
                             const input = this._ref["tipo"];
-                            input.setValue(e.descripcion)
-                            input.setData(e);
+                            this.props.staff.descripcion_staff_tipo = e.descripcion;
+                            this.props.staff.key_staff_tipo = e.key;
+                            this.forceUpdate();
+                            // input.setValue(e.descripcion)
+                            // input.setData(e);
                         }
                     })
                 }} />
@@ -197,11 +427,18 @@ class InputPosition extends Component {
                 ref={r => this._ref["cantidad"] = r}
                 type='number'
                 defaultValue={1}
+                value={this.props.staff.cantidad}
                 col={"xs-3"}
                 label={SLanguage.select({
                     es: "Cantidad",
                     en: "Quantity"
-                })} required placeholder={"0"} />
+                })} required placeholder={"0"}
+                onChangeText={e => {
+                    this.props.staff.cantidad = e;
+                    // this.forceUpdate();
+                    // this._ref["cantidad"].setValue(e)
+                }}
+            />
 
 
             <Input col={"xs-3"} inputStyle={{
@@ -210,6 +447,8 @@ class InputPosition extends Component {
                 backgroundColor: STheme.color.card,
                 color: STheme.color.text,
             }}
+
+                value={this.props.staff.nivel_ingles}
                 // infoStyle={{
                 //     color: STheme.color.text,
                 //     fontSize: 12,
@@ -235,9 +474,11 @@ class InputPosition extends Component {
                                 <InputSelect
                                     data={["NONE", "BASIC", "MEDIUM", "ADVANCED"]}
                                     onChange={val => {
-                                        if (this._ref["nivel_ingles"]) {
-                                            this._ref["nivel_ingles"].setValue(val)
-                                        }
+                                        // if (this._ref["nivel_ingles"]) {
+                                        this.props.staff.nivel_ingles = val;
+                                        this.forceUpdate();
+                                        // this._ref["nivel_ingles"].setValue(val)
+                                        // }
                                     }}
                                     ITEM_HEIGHT={30} />
                             </SView>
@@ -257,6 +498,7 @@ class InputPosition extends Component {
                 //     fontSize: 12,
                 // }}
                 required
+                value={this.props.staff.hora_inicio}
                 ref={r => this._ref["hora_inicio"] = r}
                 keyboardType="numeric"
                 label={SLanguage.select({
@@ -278,10 +520,13 @@ class InputPosition extends Component {
                                 <InputHora defaultValue={formatTime(this._ref["hora_inicio"].getValue())} onChange={val => {
                                     this.state.startTime = val;
 
-                                    if (this._ref["hora_inicio"]) {
-                                        console.log(val);
-                                        this._ref["hora_inicio"].setValue(new SDate(val, "hh:mm").toString("HH"))
-                                    }
+                                    this.props.staff.hora_inicio = new SDate(val, "hh:mm").toString("HH")
+                                    this.props.staff.fecha_inicio = new SDate(val, "hh:mm").toString("yyyy-MM-ddThh:mm:ss")
+                                    this.forceUpdate();
+                                    // if (this._ref["hora_inicio"]) {
+                                    //     console.log(val);
+                                    //     this._ref["hora_inicio"].setValue(new SDate(val, "hh:mm").toString("HH"))
+                                    // }
                                 }} />
                             </SView>
                         }
@@ -304,6 +549,7 @@ class InputPosition extends Component {
                 //     fontSize: 12,
                 // }}
                 ref={r => this._ref["hora_fin"] = r}
+                value={this.props.staff.hora_fin}
                 //   required
                 keyboardType="numeric"
                 label={SLanguage.select({
@@ -325,10 +571,13 @@ class InputPosition extends Component {
                                 <InputHora defaultValue={formatTime(this._ref["hora_fin"].getValue())} onChange={val => {
                                     this.state.endTime = val;
 
-                                    if (this._ref["hora_fin"]) {
-                                        this._ref["hora_fin"].setValue(new SDate(val, "hh:mm").toString("HH"))
-                                        // this._ref["hora_fin"].setValue(val)
-                                    }
+                                    this.props.staff.hora_fin = new SDate(val, "hh:mm").toString("HH")
+                                    this.props.staff.fecha_fin = new SDate(val, "hh:mm").toString("yyyy-MM-ddThh:mm:ss")
+                                    this.forceUpdate();
+                                    // if (this._ref["hora_fin"]) {
+                                    //     this._ref["hora_fin"].setValue(new SDate(val, "hh:mm").toString("HH"))
+                                    //     // this._ref["hora_fin"].setValue(val)
+                                    // }
                                 }} />
                             </SView>
                         }
@@ -342,6 +591,13 @@ class InputPosition extends Component {
                     <SIconApp name='Delete' />
                 </SView>
             </SView>
+            <SInput
+                ref={r => this._ref["descripcion"] = r}
+                required
+                label={"Descripcion"} type='textArea' value={this.props?.staff?.descripcion} onChangeText={e => {
+                    this.props.staff.descripcion = e;
+                    this.forceUpdate();
+                }} />
         </SView>
     }
 }   
