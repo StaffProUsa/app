b01517c96631c036ebbf5587b4efb9c83399f1b8
Licethd <liceth.dfe@gmail.com>
2025-05-26 23:26:18 -0400

    Mover el campo location detraz del nombre en el pdf del timeSheet #350

--- src/Components/PDF/index.ts
+++ src/Components/PDF/index.ts
@@ -1,7 +1,9 @@
 import timeSheet from "./timeSheet"
 import factura from "./factura"
+import history from "./history"
 const PDF  = {
     timeSheet,
-    factura
+    factura,
+    history
 }
 export default PDF
